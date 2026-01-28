60f23ea93cdb8f0fb467a85bfeb8d180aaa2e1d0
Aditya Raj <adityaraj790391@gmail.com>
2026-01-28 15:47:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,15 @@
-// Take an array with length 10 and keep 10 different student marks in btw 0 and 40 and check student marks is pass or fail.
+// const headingElement = document.getElementById("myHeading");
+// console.log(headingElement);
 
+// headingElement.textContent = "New Heading";
+// headingElement.style.color = "blue";
+// headingElement.style.backgroundColor = "yellow";
 
-// let marks = [12, 34, 25, 37, 27, 10, 33, 22, 30, 29];
-// let passedStudents = marks.filter ((x)=>{
-//     if(x>=16){
-//         console.log(x, "pass")
-//     }
-//     else{
-//         console.log(x, "fail")
-// };
+//// Select the first <p> element inside using querySelector
+const paragraph = document.querySelector("#content p");
 
-// find the sum of all the elements inside  and array with for loop.
+//// Change the text content of the selected paragraph
+paragraph.textContent = "This paragraph has been update using querySelector";
 
-// let marks = [ 98, 97, 95, 93, 92 ];
-
-//  let sum = 0;
-//  for (let val of marks) {
-//     sum += val;
-//  }
-
-// console.log(sum)
-
-
-
-// console.log([]-[]);
-// console.log(0.1 + 0.2 === 0.3);
-// console.log([]==0);
-// console.log(""==0);
-// console.log(""==[]);
-
-
-// let arr = [1,2,3,4,5];
-// let res = arr.reduce((acc, curr)=>{
-//     return acc + curr;
-// },0);
-// console.log(res);
-
-// let heroes = [ "Ironman", "SpiderMan", "Thor", "Hulk" ];
-
-// for (let i = 0; i<heroes.length; i++) {
-//     console.log(heroes[i]);
-// }
-
-
-let arr = ["Alex, 19, null, undefined, 54, 23"];
-
-for (i=0; i<arr.length; i++) {
-    console.log(arr[i]);
-}
-
-
-// console.log(arr)
-
-// console.log(arr.pop)
-// arr.pop()
-
-
-// arr[0] = "Joy"
-// arr[1] = "21"
-// arr[4] = "Victor"
-
-// console.log(arr.length);
-
-// arr.push("Red");
-// arr.push(33,57);
-
-// console.log(arr);
-
-
-// arr.pop()
-// console.log(arr);
-
-// arr.unshift("Hello");
-
-// console.log(arr);
-
-
-let array = [1,2,3,4,5]
-// let rev = []
-// while(arr.length>0) {
-//     rev.push(arr.pop())
-// }
-// console.log(rev);
-
-while(array.length>0) {
-    let val = arr.shift();
-    if (val>0) {
-        res.push(val);
-
-    }
-    console.log(val);
-}
-
-
-
- function palindrome ( ) {
-    let arrays = [1,2,3,4,5]
-    let original = []
-    let rev = [] 
-
-
-}
-
-
-// remove all duplicate elements from the array
-// split even and odd. nos. from an given array
-// move all zeros to the end of the array
-
-
-
-
-
-
-
-
+//// Change the style of  the selected paragraph
+paragraph.style.color = "green";
