460f9a96eaa77b86993bb4f762bbe4136df4cd81
Aditya Raj <adityaraj790391@gmail.com>
2026-01-28 15:49:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-// const headingElement = document.getElementById("myHeading");
-// console.log(headingElement);
+/// we can access to individual value by targeting 
 
-// headingElement.textContent = "New Heading";
-// headingElement.style.color = "blue";
-// headingElement.style.backgroundColor = "yellow";
+/// execution has two phases global execution and functional execution context
+/// global execution is divided into creation and execution phase
+/// functional execution is divided into creation and execution phase
 
-//// Select the first <p> element inside using querySelector
-const paragraph = document.querySelector("#content p");
 
-//// Change the text content of the selected paragraph
-paragraph.textContent = "This paragraph has been update using querySelector";
+var a = 10;
+console.log(a)
+function demo () {
+    let b =20;
+    console.log(b);
+}
+demo();
 
-//// Change the style of  the selected paragraph
-paragraph.style.color = "green";
+
+//// CallStack is a process and mechanism which keeps track of every process running in the background or javascript course.
+//// CallStack stores global execution context and functional execution context
+//// After the processing of the code it will pop out the FEC then GEC at the end.
+
+
+
+let user =  {
+    FullNmae: "Demon King",
+    address: "Gurugram",
+    mobileno: 123456789,
+    favcolor: ["black", "white"]
+}
+
+console.log(user.FullNmae, user.address, user.mobileno);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(user.favcolor[1]);
+console.log(Object.entries(user));
+
+
+const car = {
+    make: "Toyota",
+    model: "Thar"
+}
+// Object.freeze(car);
+Object.seal(car);
+car.model = "XEV9E";
+car.color = "Blue";
+console.log(car);
+
+
+//// Object Methods -
+//// The difference between object.freeze and object.seal
+//// What is execution context and how JavaScript code executes in execution context.
+//// What is CallStack and how CallStack works in JavaScript.
+//// Difference between DOM and HTML.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
